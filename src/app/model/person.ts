2501ccf6456baacb03cdd0ce43ff294e08946ab1
Kacper <kacperadamiak14@gmail.com>
2024-08-03 14:55:03 +0200

    Added person module

---
+++ src/app/model/person.ts
@@ -0,0 +1,8 @@
+export interface Person {
+  id?: number
+  name: string
+  surname: string
+  day: number
+  month: number
+  year: number
+}
---
+++ src/app/service/person.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import {Person} from "../model/person";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonService {
+
+  private persons: Person[] = [
+    {name: 'John', surname: 'Doe', day: 3, month: 8, year: 2013},
+    {name: 'Jane', surname: 'Doe', day: 15, month: 3, year: 1995},
+    {name: 'John', surname: 'Smith', day: 10, month: 7, year: 1980},
+    {name: 'John', surname: 'Smith', day: 10, month: 7, year: 1980},
+    {name: 'John', surname: 'Smith', day: 10, month: 7, year: 1980},
+    {name: 'John', surname: 'Smith', day: 10, month: 7, year: 1980},
+  ]
+
+  constructor() { }
+
+  getPersons(): Person[] {
+    return this.persons;
+  }
+
+  getPerson(id: number): Person | null {
+    return this.persons.find(p => p.id === id) ?? null;
+  }
+
+  addPerson(person: Person) {
+    this.persons.push(person);
+  }
+
+  deletePerson(person: Person) {
+    this.persons = this.persons.filter(p => p !== person);
+  }
+
+  updatePerson(person: Person) {
+    this.persons = this.persons.map(p => p === person ? person : p);
+  }
+
+  getPersonsByDate(date: Date): Person[] | null {
+    return this.persons.filter(p => p.day === date.getDay() && p.month === date.getMonth() && p.year === date.getFullYear());
+  }
+
+  getPersonAge(person: Person, year: number): number {
+    return year - person.year;
+  }
+
+}
